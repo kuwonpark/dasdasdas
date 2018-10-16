2bc779827a7fb0ecd0ff0d8280f222c2718ac9a2
KOITT <KOITT@KOITT-PC>
2018-10-16 21:19:13 +0900

    dasdsa

--- src/main/java/com/bdi/sp/dao/daoImpl/DepartDAOImpl.java
+++ src/main/java/com/bdi/sp/dao/daoImpl/DepartDAOImpl.java
@@ -2,38 +2,46 @@ package com.bdi.sp.dao.daoImpl;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.bdi.sp.dao.DepartDAO;
 
+@Repository
 public class DepartDAOImpl implements DepartDAO {
 
+	@Autowired
+	public SqlSession ss;
+	
 	@Override
 	public List<com.bdi.sp.vo.Depart> DepartList(com.bdi.sp.vo.Depart d) {
 		// TODO Auto-generated method stub
-		return null;
+		return ss.selectList("com.bdi.sp.DepartMapper.selectDepartList",d);
 	}
 
 	@Override
 	public com.bdi.sp.vo.Depart Depart(int dino) {
 		// TODO Auto-generated method stub
-		return null;
+		return ss.selectOne("com.bdi.sp.DepartMapper.selectDepart",dino);
 	}
 
 	@Override
 	public int insertDepart(com.bdi.sp.vo.Depart d) {
 		// TODO Auto-generated method stub
-		return 0;
+		return ss.insert("com.bdi.sp.DepartMapper.updateDepart",d);
 	}
 
 	@Override
 	public int deleteDepart(int dino) {
 		// TODO Auto-generated method stub
-		return 0;
+		return ss.delete("com.bdi.sp.DepartMapper.selectDepartList",dino);
 	}
 
 	@Override
 	public int updateDepart(com.bdi.sp.vo.Depart d) {
 		// TODO Auto-generated method stub
-		return 0;
+		return ss.update("com.bdi.sp.DepartMapper.selectDepartList",d);
 	}
 
 }
